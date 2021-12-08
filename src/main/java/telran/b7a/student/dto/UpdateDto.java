cedfe8d1bb755883b574ad28b6a19c9931edc68f
dmitrii <dmitrii@aser-laptop>
2021-12-08 22:27:55 +0200

    First commit

---
+++ src/main/java/telran/b7a/student/dto/UpdateDto.java
@@ -0,0 +1,16 @@
+package telran.b7a.student.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class UpdateDto {
+	String name;
+	String password;
+
+}
