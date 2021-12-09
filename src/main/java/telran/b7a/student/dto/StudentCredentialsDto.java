21bb52589840a2068f9563d3f730288b50962cac
dmitrii <dmitrii@aser-laptop>
2021-12-09 10:52:22 +0200

    Rename AddStudent

--- src/main/java/telran/b7a/student/dto/StudentCredentialsDto.java
+++ src/main/java/telran/b7a/student/dto/StudentCredentialsDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 @Builder
-public class AddStudentDto {
+public class StudentCredentialsDto {
 	Integer id;
 	String name;
 	String password;
